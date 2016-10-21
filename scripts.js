a413b26f502f18cd34d7ab3090c57db7d172018c
SaskiavanHeycop <saskiavanheycop@gmail.com>
2016-10-21 20:17:38 +0200

    checkpoint

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 	var obj = document.createElement("audio");
         obj.src="https://kahimyang.com/resources/sound/click.mp3";
-        obj.volume=0.10;
+        obj.volume=1.0;
         obj.autoPlay=false;
         obj.preLoad=true;
 		
@@ -18,10 +18,6 @@ $(document).ready(function () {
 								"Tas inpakken":"naarschool.html",
 								"Naar school":"eindochtend.html",
 								"Eind ochtend":"index.html",
-								"Pyjama aantrekken":"tandenpoetsenavond.html",
-								"Tanden poetsen avond":"naarbed.html",
-								"Naar bed":"eindavond.html",
-								"Eind avond":"index.html",
 							};
 			var next_page = dictionary[document.title];
 			window.open(next_page,"_self")
